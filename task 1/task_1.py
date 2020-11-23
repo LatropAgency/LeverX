832cd7f8891823615e0f215e2194d4e73cc1bbb7
LatropAgency <vovakolesnikovich@gmail.com>
2020-11-23 21:56:35 +0300

    task 1

---
+++ task 1/task_1.py
@@ -0,0 +1,75 @@
+import json
+import sys
+from dicttoxml import dicttoxml
+
+
+class Serializer:
+
+    def serialize(self, obj):
+        raise NotImplementedError
+
+
+class JsonSerializer(Serializer):
+
+    def serialize(self, obj):
+        with open('result.json', 'w') as file:
+            json.dump(obj, file, indent=4)
+
+
+class XmlSerializer(Serializer):
+
+    def serialize(self, obj):
+        my_item_func = lambda x: 'room'
+        xml = dicttoxml(obj, custom_root='rooms', attr_type=False, item_func=my_item_func)
+        with open('result.xml', 'w') as file:
+            file.write(xml.decode('utf-8'))
+
+
+class Student:
+    def __init__(self, id: int, name: str, room: int):
+        self.id = id
+        self.name = name
+        self.room = room
+
+
+class Room:
+    serializer = None
+    instances = []
+
+    def __init__(self, id: int, name: str):
+        self.id = id
+        self.name = name
+        self.students = []
+        self.__class__.instances.append(self.__dict__)
+
+    @classmethod
+    def save(cls):
+        cls.serializer.serialize(cls.instances)
+
+    def add(self, student: Student):
+        self.students.append(student.__dict__)
+
+
+if __name__ == '__main__':
+    rooms = {}
+
+    students_file_path = sys.argv[1]
+    rooms_file_path = sys.argv[2]
+    serializer_type = sys.argv[3]
+
+    if serializer_type == 'json':
+        Room.serializer = JsonSerializer()
+    elif serializer_type == 'xml':
+        Room.serializer = XmlSerializer()
+
+    with open(rooms_file_path, 'r') as f:
+        objects = json.loads(f.read())
+        for room in objects:
+            rooms[room['id']] = Room(room['id'], room['name'])
+
+    with open(students_file_path, 'r') as f:
+        objects = json.loads(f.read())
+        for student in objects:
+            rooms.get(student['room']).add(Student(student['id'], student['name'], student['room']))
+
+    Room.save()
