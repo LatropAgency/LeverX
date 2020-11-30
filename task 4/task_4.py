0d91a74afa83c318e8b5980ba9022528d20748e0
LatropAgency <vovakolesnikovich@gmail.com>
2020-11-30 03:49:12 +0300

    added read_objects and join from task 1

--- task 4/task_4.py
+++ task 4/task_4.py
@@ -1,25 +1,25 @@
-import argparse
 import json
-from serializer import JsonSerializer, XmlSerializer
-from dao import StudentDAO, RoomDAO
+import argparse
 import mysql.connector
+from serializers import JsonSerializer, XmlSerializer
+from dao import StudentDAO, RoomDAO
+from models import Student, Room
 
 
-def get_objects(file_path: str):
-    with open(file_path, 'r') as f:
-        return {x['id']: x for x in json.loads(f.read())}
+def read_objects(path: str, cls: type) -> dict:
+    """read objects from json file"""
+    with open(path, 'r') as f:
+        students = json.loads(f.read())
+        return {student['id']: cls(**student) for student in students}
 
 
-def join(rooms: dict, students: dict):
-    for id, student in students.items():
-        lst = rooms[student['room']].get('students', [])
-        if not lst:
-            rooms[student['room']]['students'] = lst
-        lst.append(student)
+def join(rooms: dict, students: dict) -> None:
+    """join rooms and students dicts"""
+    for student in students.values():
+        rooms.get(student.room).add(student)
 
 
 if __name__ == '__main__':
-    serializer = None
     rooms = {}
     students = {}
     parser = argparse.ArgumentParser()
@@ -36,21 +36,20 @@ if __name__ == '__main__':
         'password': args.password,
     }
     if args.serializer == 'json':
-        serializer = JsonSerializer()
+        Room.serializer = JsonSerializer()
     elif args.serializer == 'xml':
-        serializer = XmlSerializer()
-    rooms = get_objects('rooms.json')
-    students = get_objects('students.json')
+        Room.serializer = XmlSerializer()
+    rooms = read_objects(args.rooms, Room)
+    students = read_objects(args.students, Student)
     join(rooms, students)
-    serializer.serialize(rooms)
+    Room.save(rooms.values())
     with mysql.connector.connect(**config) as connection:
-        if connection.is_connected():
-            rooms_dao = RoomDAO(connection)
-            students_dao = StudentDAO(connection)
-            rooms_dao.insert(rooms)
-            students_dao.insert(students)
-            print(rooms_dao.rooms_list_with_student_count())
-            print(rooms_dao.top_5_rooms_with_min_avg_age_of_students())
-            print(rooms_dao.top_5_rooms_with_max_students_age_diff())
-            print(rooms_dao.rooms_with_males_and_females())
-            students_dao.create_index()
+        rooms_dao = RoomDAO(connection)
+        students_dao = StudentDAO(connection)
+        rooms_dao.insert(rooms)
+        students_dao.insert(students)
+        print(rooms_dao.rooms_list_with_student_count())
+        print(rooms_dao.top_5_rooms_with_min_avg_age_of_students())
+        print(rooms_dao.top_5_rooms_with_max_students_age_diff())
+        print(rooms_dao.rooms_with_males_and_females())
+        students_dao.create_index()
